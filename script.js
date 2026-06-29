39776cfa0f559c713fab2b576ec2092cf2d328dd
Aline Carvalho <alinecarvalhoods@gmail.com>
2026-06-29 18:24:31 -0300

    Adiciona modo claro e escuro

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 // Formulário
 
+// Botão do tema
+
+const botaoTema = document.getElementById("tema");
+botaoTema.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+
+    botaoTema.textContent = "☀️";
+
+} else {
+
+    botaoTema.textContent = "🌙";
+
+}
+
+});
+
 const formulario = document.getElementById("formulario");
 
 formulario.addEventListener("submit", function(event) {
