6517abad05ebd45d181ebd8235e4a65d90f1d1bb
Aline Carvalho <alinecarvalhoods@gmail.com>
2026-06-29 01:32:27 -0300

    Primeira versão do portfólio

---
+++ script.js
@@ -0,0 +1,29 @@
+// Formulário
+
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+   const nome = document.getElementById("nome").value.trim();
+
+   const email = document.getElementById("email").value.trim();
+
+   const mensagem = document.getElementById("mensagem").value.trim();
+
+    if (nome === "" || email === "" || mensagem === "") {
+
+       alert("Por favor, preencha todos os campos.");
+
+    return;
+
+    }
+
+    alert("Mensagem enviada com sucesso!");
+
+    formulario.reset();
+
+
+});
+
